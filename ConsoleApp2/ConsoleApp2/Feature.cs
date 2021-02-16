ee0ec890a4ec40af580f7390fd1aa924023aad2b
Ivan <ivan.serdyukov270@gmail.com>
2021-02-16 11:19:26 +0300

    Added Add method

--- ConsoleApp2/ConsoleApp2/Feature.cs
+++ ConsoleApp2/ConsoleApp2/Feature.cs
@@ -6,5 +6,12 @@ namespace ConsoleApp2
 {
     class Feature
     {
+        public int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
